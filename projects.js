365287f2bee2d7b40e9bc6de509f9aac68ea957b
Marianna <mariannadek@gmail.com>
2023-06-17 10:19:39 +0400

    HW1 vue.js

---
+++ projects.js
@@ -0,0 +1,27 @@
+const projectsData = `[
+      {
+        "name": "Product1",
+        "heading": "Modern Kitchan",
+        "description": "Decor / Artchitecture",
+        "image": "Product1.svg"
+      },
+      {
+        "name": "Product2",
+        "heading": "Modern Kitchan",
+        "description": "Decor / Artchitecture",
+        "image": "Product2.svg"
+      },
+      {
+        "name": "Product3",
+        "heading": "Modern Kitchan",
+        "description": "Decor / Artchitecture",
+        "image": "Product3.svg"
+      },
+      {
+        "name": "Product4",
+        "heading": "Modern Kitchan",
+        "description": "Decor / Artchitecture",
+        "image": "Product4.svg"
+      }
+    ]
+  `
